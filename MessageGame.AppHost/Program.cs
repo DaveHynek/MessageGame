a903291d144cb0da88c6f9dcb12e73bfe16dded8
Dave Hynek <david.alan.hynek@gmail.com>
2024-08-21 21:59:32 -0400

    Initial Commit
    Adding initial .Net Aspire and Angular projects to the repository.

---
+++ MessageGame.AppHost/Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var weatherApi = builder.AddProject<Projects.MessageGame_MinimalApi>("weatherapi")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("angular", "../MessageGame.Angular")
+    .WithReference(weatherApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.Build().Run();
